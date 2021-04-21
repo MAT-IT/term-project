a73d0c251246d47cff2df85d17c84f109b40c4a1
matzhd <77358430+matzhd@users.noreply.github.com>
2021-04-21 12:52:09 +0100

    added searching area

---
+++ src/head/Searchform.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Searchform() {
+    return (
+        <div>
+            <form>
+                <input> </input>
+            </form>
+        </div>
+    )
+}
